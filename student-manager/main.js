75aa5425228ba41a9a166ac23a5804bcfefa1558
Guillermo <g.rdz.jauregui06@gmail.com>
2025-05-22 11:14:22 -0600

    si

--- student-manager/main.js
+++ student-manager/main.js
@@ -3,34 +3,25 @@
 let students = [];
 
 function addStudent() {
-  console.log("Agregar estudiante");
   const name = document.getElementById("nameInput").value.trim();
-  const grade = parseFloat(document.getElementById("gradeInput").value());
-  if (name === "" || isNaN(grade) || (grade <= 0 && grade >= 100)) {
-    alert("Please enter a valid data");
-    return;
-  }
-  const student = {
-    name,
-    grade,
-    status: grade >= 70 ? "passed" : "failed",
-  };
+  const grade = parseFloat(document.getElementById("gradeInput").value);
 
+  const student = { name, grade };
   students.push(student);
 
-  dislayStudents();
-  document.getElementById("nameInput").value = "";
-  document.getElementById("gradeInput").value = "";
+  displayStudents();
+  updateAverage();
 }
 
 function displayStudents() {
   const list = document.getElementById("studentList");
+  list.innerHTML = ``;
 
   for (let i = 0; i < students.length; i++) {
     const li = document.createElement("li");
     li.innerHTML = `
-              <p> ${students[i].name} - ${students[i].grade} - ${students[i].status}</p> 
-            `;
+      <p>${students[i].name} - ${students[i].grade} </p>
+    `;
     list.appendChild(li);
   }
 }
@@ -46,4 +37,8 @@ function updateAverage() {
   let average = total / students.length;
 
   document.getElementById("averageDisplay").textContent = `Average Grade: ${average.toFixed(2)}`;
+}
+
+function saveToLocalStorage() {
+
 }
